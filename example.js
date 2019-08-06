e80c31f71eaee76805c5a83f7e4c57fe96b2c650
Nevill <nevill.dutt@gmail.com>
2019-08-06 15:15:45 +0800

    Implement method batchGet of rawkv client

--- example.js
+++ example.js
@@ -9,9 +9,17 @@ let address = 'pd0:2379';
   console.log('Cluster ID:', id.toString());
 
   await client.put('company', 'PingCAP');
+  await client.put('fruit', 'Apple');
 
   let company = await client.get('company');
-  console.log('Company:', company);
+  console.log('Get company:', company);
+
+  let fruit = await client.get('fruit');
+  console.log('Get fruit:', fruit);
+
+  let values = await client.batchGet(['company', 'fruit']);
+  console.log('BatchGet:', values);
 
   await client.delete('company');
+  await client.delete('fruit');
 })();
